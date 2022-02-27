c2b2535e3a614ac2e0b3b14d38324edcda3d9914
Sunny <wuszuyu791010@gmail.com>
2022-02-27 15:33:38 +1000

    feat: add readme

---
+++ README.js
@@ -0,0 +1,62 @@
+<h1>URL Shortener</h1>
+<h2>Introduction</h2>  
+<p>
+  If you need to shorten your URL, please feel free to use it!
+</p>
+
+<h2>
+  Function
+</h2>
+<ol>
+  <li>Create the shorten URL for your URL</li>
+  <li>You can click the COPY button to copy the shorten URL</li>
+  <li>Access your original URL by using the shorten URL</li>
+  <li>Run locally</li>
+</ol>
+<h2>
+  Install node.js and npm
+
+</h2>
+<p>
+  Clone this repository to create a local copy on your computer
+
+</p>
+<p>
+
+  Open the repository on your computer, and enter the folder by your local terminal. 
+</p>
+<p>
+
+  Input :
+  npm install
+  After installation, 
+  Input:
+  
+  npm run start
+  Open browser to get into the url below:
+  
+  The server is listening on http://localhost:3000
+  mongodb connected!
+  If you want to stop it:
+  
+  ctrl + c
+</p>
+
+<h2>
+  Development Tools
+
+</h2>
+<ol>
+  <li>Express 4.17.3</li>
+  <li>Express-Handlebars 4.0.2</li>
+  <li>Bootstrap 5.0.2</li>
+  <li>mongoose 6.2.2</li>
+  <li>promised-url-exists 1.0.7</li>
+  <li>method-override 3.0.0</li>
+</ol>
+
+
+
+
+
+
